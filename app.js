1bdf5e78b9431941e45c59fb322c546def57e935
SyedDanishRaza <dr3822632@gmail.com>
2025-08-30 10:36:51 -0700

    update

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
 console.log("JavaScript Connected!");
 
-var screen = document.getElementById("screen");
+var calculate = document.getElementById("calculation");
+var answer = document.getElementById("result");
 
 function clickbtn(value){
-    screen.value += value
+    if(answer.value === "Error"){
+        calculate.value = "";
+        answer.value = "";
+    }
+    calculate.value += value;
+    answer.value = "";
 }
+
 function empty(){
-    screen.value = ""
+    calculate.value = "";
+    answer.value = "";
 }
+
 function delete1val(){
-    screen.value = screen.value.slice(0,-1)
+    if( answer.value === "Error"){
+        calculate.value = "";
+        answer.value = "";
+    } else {
+        calculate.value = calculate.value.slice(0,-1);
+        answer.value = "";
+    }
 }
-function Answer(){
-    screen.value = eval(screen.value)
+
+function ans(){
+    var exp = calculate.value;
+    var opt = "-+*/%";
+    var error = false;
+
+    if(exp === "") error = true;
+
+    if(opt.indexOf(exp[0]) !== -1 || opt.indexOf(exp[exp.length-1]) !== -1) error = true;
+
+    for(var i=0; i<exp.length-1; i++){
+        if(opt.indexOf(exp[i]) !== -1 && opt.indexOf(exp[i+1]) !== -1){
+            error = true;
+            break;
+        }
+    }
+
+    if(error){
+        answer.value = "Error";
+        return;
+    }
+
+    let result = eval(exp);
+    if(isNaN(result)){
+        answer.value = "Error";
+    } else {
+        answer.value = result;
+    }
 }
